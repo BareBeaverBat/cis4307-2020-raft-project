f1594ed514f22bdf7ca18a5eafc2b9fa632207c3
BareBeaverBat <ssiliss72@gmail.com>
2020-04-14 17:28:24 -0400

    add logic for initializing a set of nodes from config file
    including definition of node service constructor which parses rest of config file to know about rest of system

--- raftnode.py
+++ raftnode.py
@@ -1,5 +1,15 @@
+import logging
+
 import rpyc
 import sys
+
+
+class NodeRef:
+	def __init__(self, name, hostAddr, port):
+		self.name=  name
+		self.host = hostAddr
+		self.port = port
+
 
 '''
 A RAFT RPC server class.
@@ -10,14 +20,27 @@ other methods to implement ONLY the leader election part of the RAFT
 protocol.
 '''
 class RaftNode(rpyc.Service):
-	
 
 	"""
         Initialize the class using the config file provided and also initialize
         any datastructures you may need.
 	"""
-	def __init__(self, config):
-		pass
+	def __init__(self, configFileName, nodeIdentityIndex):
+		self.identityIndex = nodeIdentityIndex
+		self.otherNodes = []
+		with open(configFileName) as nodesConfigFile:
+			nodesConfigFile.readline() #ignore first line with node count
+
+			for nodeInd, nodeLine in enumerate(nodesConfigFile):
+				if nodeInd != nodeIdentityIndex:
+					nodeTerms = nodeLine.split(":")
+					otherNodeName = nodeTerms[0].strip()
+					otherNodeHost = nodeTerms[1].strip()
+					otherNodePort = nodeTerms[2].strip()
+					otherNodePort = int(otherNodePort)
+					otherNode = NodeRef(otherNodeName, otherNodeHost, otherNodePort)
+					self.otherNodes.append(otherNode)
+
 
 	'''
         x = is_leader(): returns True or False, depending on whether
@@ -34,6 +57,32 @@ class RaftNode(rpyc.Service):
 
 if __name__ == '__main__':
 	from rpyc.utils.server import ThreadPoolServer
-	server = ThreadPoolServer(RaftNode(sys.argv[1]), port = 6000)
-	server.start()
+
+	nodeNum =-1
+	nodePorts= []
+
+	configFileName = sys.argv[1]
+	with open(configFileName) as configFile:
+		nodeNumLine = configFile.readline()
+		if nodeNumLine[:2] == "N:":
+			nodeNumStr = nodeNumLine[2:]
+			nodeNumStr = nodeNumStr.strip()
+			nodeNum= int(nodeNumStr)
+		else:
+			logging.critical("invalid config file- bad initial node count line: ", nodeNumLine)
+			raise Exception("bad config file")
+
+		for nodeLine in configFile:
+			nodeTerms = nodeLine.split(":")
+			nodePortStr = nodeTerms[2].strip()
+			nodePortVal = int(nodePortStr)
+			nodePorts.append(nodePortVal)
+
+
+	if(nodeNum > 0):
+		for nodeInd, nodePort in enumerate(nodePorts):
+			server = ThreadPoolServer(RaftNode(configFileName, nodeInd), port=nodePort)
+			server.start()
+
+
 
