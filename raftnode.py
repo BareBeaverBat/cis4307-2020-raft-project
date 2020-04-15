93002e20c4418802db57d0fb89a63712e4fdf1f7
BareBeaverBat <ssiliss72@gmail.com>
2020-04-14 20:06:11 -0400

    undid indenting change

--- raftnode.py
+++ raftnode.py
@@ -5,10 +5,10 @@ import sys
 
 
 class NodeRef:
-	def __init__(self, name, hostAddr, port):
-		self.name = name
-		self.host = hostAddr
-		self.port = port
+    def __init__(self, name, hostAddr, port):
+        self.name = name
+        self.host = hostAddr
+        self.port = port
 
 
 '''
@@ -22,81 +22,84 @@ protocol.
 
 
 class RaftNode(rpyc.Service):
-	"""
-		Initialize the class using the config file provided and also initialize
-		any datastructures you may need.
-	"""
-	
-	def __init__(self, configFilePath, nodeIdentityIndex):
-		self.identityIndex = nodeIdentityIndex
-		self.otherNodes = []
-		with open(configFilePath) as nodesConfigFile:
-			nodesConfigFile.readline()  # ignore first line with node count
-			
-			for nodeInd, nodeLine in enumerate(nodesConfigFile):
-				if nodeInd != nodeIdentityIndex:
-					otherNodeTerms = nodeLine.split(":")
-					otherNodeName = otherNodeTerms[0].strip()
-					otherNodeHost = otherNodeTerms[1].strip()
-					otherNodePort = otherNodeTerms[2].strip()
-					otherNodePort = int(otherNodePort)
-					otherNode = NodeRef(otherNodeName, otherNodeHost, otherNodePort)
-					self.otherNodes.append(otherNode)
-		
-		print("I am node %d and I just finished being constructed, with %d fellow nodes",
-			  self.identityIndex, len(self.otherNodes))
-	
-	'''
-		x = is_leader(): returns True or False, depending on whether
-		this node is a leader
+    """
+        Initialize the class using the config file provided and also initialize
+        any datastructures you may need.
+    """
 
-		As per rpyc syntax, adding the prefix 'exposed_' will expose this
-		method as an RPC call
+    def __init__(self, configFilePath, nodeIdentityIndex):
+        self.identityIndex = nodeIdentityIndex
+        nodeName = "node" + str(nodeIdentityIndex)
+        nodeLogger = None
 
-		CHANGE THIS METHOD TO RETURN THE APPROPRIATE RESPONSE
-	'''
-	
-	def exposed_is_leader(self):
-		return False
+        self.otherNodes = []
+        with open(configFilePath) as nodesConfigFile:
+            nodesConfigFile.readline()  # ignore first line with node count
+
+            for nodeInd, nodeLine in enumerate(nodesConfigFile):
+                if nodeInd != nodeIdentityIndex:
+                    otherNodeTerms = nodeLine.split(":")
+                    otherNodeName = otherNodeTerms[0].strip()
+                    otherNodeHost = otherNodeTerms[1].strip()
+                    otherNodePort = otherNodeTerms[2].strip()
+                    otherNodePort = int(otherNodePort)
+                    otherNode = NodeRef(otherNodeName, otherNodeHost, otherNodePort)
+                    self.otherNodes.append(otherNode)
+
+        print("I am node %d and I just finished being constructed, with %d fellow nodes",
+              self.identityIndex, len(self.otherNodes))
+
+    '''
+        x = is_leader(): returns True or False, depending on whether
+        this node is a leader
+    
+        As per rpyc syntax, adding the prefix 'exposed_' will expose this
+        method as an RPC call
+    
+        CHANGE THIS METHOD TO RETURN THE APPROPRIATE RESPONSE
+    '''
+
+    def exposed_is_leader(self):
+        return False
 
 
 if __name__ == '__main__':
-	from rpyc.utils.server import ThreadPoolServer
-	
-	nodeNum = -1
-	currNodePort = -1
-	
-	configFileName = sys.argv[1]
-	
-	currNodeIndexStr = sys.argv[2]
-	currNodeIndex = int(currNodeIndexStr)
-	
-	with open(configFileName) as configFile:
-		nodeNumLine = configFile.readline()
-		if nodeNumLine[:2] == "N:":
-			nodeNumStr = nodeNumLine[2:]
-			nodeNumStr = nodeNumStr.strip()
-			nodeNum = int(nodeNumStr)
-		else:
-			logging.critical("invalid config file- bad initial node count line: %s", nodeNumLine)
-			raise Exception("bad config file")
-		
-		if currNodeIndex < nodeNum:
-			nodeDescriptions = configFile.readlines()
-			if len(nodeDescriptions) == nodeNum:
-				currNodeLine = nodeDescriptions[currNodeIndex]
-				nodeTerms = currNodeLine.split(":")
-				nodePortStr = nodeTerms[2].strip()
-				currNodePort = int(nodePortStr)
-			else:
-				logging.critical("invalid config file- wrong number of lines of node descriptions", nodeNumLine)
-				raise Exception("bad config file")
-		else:
-			# todo figure out how to log properly
-			logging.critical("unacceptably high index %d for node system which only has %d nodes", currNodeIndex,
-							 nodeNum)
-			raise Exception("bad node index")
-	
-	if (currNodePort > 0):
-		server = ThreadPoolServer(RaftNode(configFileName, currNodeIndex), port=currNodePort)
-		server.start()
+    from rpyc.utils.server import ThreadPoolServer
+
+    nodeNum = -1
+    currNodePort = -1
+
+    configFileName = sys.argv[1]
+
+    currNodeIndexStr = sys.argv[2]
+    currNodeIndex = int(currNodeIndexStr)
+
+    with open(configFileName) as configFile:
+        nodeNumLine = configFile.readline()
+        if nodeNumLine[:2] == "N:":
+            nodeNumStr = nodeNumLine[2:]
+            nodeNumStr = nodeNumStr.strip()
+            nodeNum = int(nodeNumStr)
+        else:
+            logging.critical("invalid config file- bad initial node count line: %s", nodeNumLine)
+            raise Exception("bad config file")
+
+        if currNodeIndex < nodeNum:
+            nodeDescriptions = configFile.readlines()
+            if len(nodeDescriptions) == nodeNum:
+                currNodeLine = nodeDescriptions[currNodeIndex]
+                nodeTerms = currNodeLine.split(":")
+                nodePortStr = nodeTerms[2].strip()
+                currNodePort = int(nodePortStr)
+            else:
+                logging.critical("invalid config file- wrong number of lines of node descriptions", nodeNumLine)
+                raise Exception("bad config file")
+        else:
+            # todo figure out how to log properly
+            logging.critical("unacceptably high index %d for node system which only has %d nodes", currNodeIndex,
+                             nodeNum)
+            raise Exception("bad node index")
+
+    if (currNodePort > 0):
+        server = ThreadPoolServer(RaftNode(configFileName, currNodeIndex), port=currNodePort)
+        server.start()
