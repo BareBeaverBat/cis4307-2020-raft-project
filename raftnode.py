4fc13fef1e54c97ec53ed2a3046bf100c2b9330f
BareBeaverBat <ssiliss72@gmail.com>
2020-04-15 10:54:20 -0400

    lay groundwork for most of project reqs
    Add most of logic for rpcs and for calling rpc's
    Add most of logic for saving node state on persistent storage
    Add random election timeout mechanism
    Start implementation of heartbeats
    Start implementation of elections

--- raftnode.py
+++ raftnode.py
@@ -1,7 +1,13 @@
 import logging
+import os
+import random
+import threading
+import time
 
 import rpyc
 import sys
+
+
 
 
 class NodeRef:
@@ -9,6 +15,11 @@ class NodeRef:
         self.name = name
         self.host = hostAddr
         self.port = port
+
+class RpcReturn:
+    def __init__(self, termNum, outcomeBoolean):
+        self.term = termNum
+        self.boolResult = outcomeBoolean
 
 
 '''
@@ -22,6 +33,26 @@ protocol.
 
 
 class RaftNode(rpyc.Service):
+    ELECTION_TIMEOUT_BASELINE=0.150 #seconds to wait before calling an election
+    HEARTBEAT_INTERVAL= ELECTION_TIMEOUT_BASELINE/5
+    NODE_STATE_FOLDER = "node_states"
+
+    def _constructNodeStateFilePath(self):
+        pathStr = os.path.join(RaftNode.NODE_STATE_FOLDER, "node" + self.identityIndex + ".txt")
+        return pathStr
+
+    def _saveNodeState(self):
+        nodeStateStorageFilePath = self._constructNodeStateFilePath()
+        with open(nodeStateStorageFilePath, mode="w") as nodeStateStorageFile:
+            nodeState = "term= %d\n" % self.currTerm
+            if self.voteTarget is not None:
+                nodeState += "vote=%d\n" % self.voteTarget
+            nodeStateStorageFile.write(nodeState)
+            nodeStateStorageFile.flush()
+            os.fsync(nodeStateStorageFile.fileno())
+
+
+
     """
         Initialize the class using the config file provided and also initialize
         any datastructures you may need.
@@ -34,7 +65,8 @@ class RaftNode(rpyc.Service):
         nodeName = "raftNode_" + str(nodeIdentityIndex)
         self.nodeLogger = logging.getLogger(nodeName)
         self.nodeLogger.setLevel(logging.DEBUG)
-        logFileHandler = logging.FileHandler(nodeName + ".log")
+        logFilePath = os.path.join("node_logs", nodeName + ".log")
+        logFileHandler = logging.FileHandler(logFilePath)
         logFileHandler.setLevel(logging.DEBUG)
         consoleHandler = logging.StreamHandler()
         consoleHandler.setLevel(logging.WARN)
@@ -44,6 +76,8 @@ class RaftNode(rpyc.Service):
         self.nodeLogger.addHandler(logFileHandler)
         self.nodeLogger.addHandler(consoleHandler)
 
+        #todo check for stored node state that can be recovered
+        #todo? do I need to restore leader status?
 
         self.otherNodes = []
         with open(configFilePath) as nodesConfigFile:
@@ -59,8 +93,22 @@ class RaftNode(rpyc.Service):
                     otherNode = NodeRef(otherNodeName, otherNodeHost, otherNodePort)
                     self.otherNodes.append(otherNode)
 
+        self.isCandidate = False
+        self._leaderStatus = False
+        self.currTerm = 0
+        self.voteTarget = None # who the node is voting for in the current term
+
+        self.electionTimeout = (1+random.random())*RaftNode.ELECTION_TIMEOUT_BASELINE
+        self.lastContactTimestamp = time.time()
+        self._lastContactTimestampLock = threading.Lock()
+
+        electionTimer = threading.Timer(self.electionTimeout, self.check_for_election_timeout)
+        electionTimer.start()
+
         self.nodeLogger.debug("I am node %d and I just finished being constructed, with %d fellow nodes",
               self.identityIndex, len(self.otherNodes))
+
+
 
     '''
         x = is_leader(): returns True or False, depending on whether
@@ -73,7 +121,121 @@ class RaftNode(rpyc.Service):
     '''
 
     def exposed_is_leader(self):
-        return False
+        return self._leaderStatus
+
+    def exposed_append_entries(self, requesterTerm, leaderIndex):
+        if(requesterTerm < self.currTerm):
+            self.nodeLogger.info("in term %d, received append_entries() from stale leader %d which thought it was in term %d", self.currTerm, leaderIndex, requesterTerm)
+            return RpcReturn(self.currTerm, False)
+
+        #todo update if term >
+
+        self.nodeLogger.debug("in term %d, executing append_entries on behalf of node %d, the leader in term %d",
+                              self.currTerm, leaderIndex, requesterTerm)
+        with self._lastContactTimestampLock:
+            self.lastContactTimestamp = time.time()
+
+        #todo finish?
+
+    def call_append_entries(self, otherNode):
+        assert self.exposed_is_leader()
+        nodeConn = rpyc.connect(otherNode.host, otherNode.port)
+        appendEntriesRetVal = nodeConn.append_entries(self.currTerm, self.identityIndex)
+        return appendEntriesRetVal
+
+
+
+
+    def exposed_request_vote(self, requesterTerm, candidateIndex):
+        willVote = False
+
+        if requesterTerm < self.currTerm:
+            self.nodeLogger.info(
+                "in term %d, received request_vote() from stale leader %d which thought it was in term %d",
+                self.currTerm, candidateIndex, requesterTerm)
+        else:
+            with self._lastContactTimestampLock:
+                self.lastContactTimestamp = time.time()
+
+            if requesterTerm > self.currTerm:
+                if self.voteTarget is not None:
+                    self.nodeLogger.warning("was in election for term %d, voting for candidate node %d, "
+                                            "when received request for vote in later term %d", self.currTerm,
+                                            self.voteTarget, requesterTerm)
+                    self.voteTarget = None
+                # cast vote here?
+
+                self.isCandidate = False
+                self._leaderStatus = False
+                self.currTerm = requesterTerm
+
+
+            else:
+                assert not self.exposed_is_leader()
+
+            if  self.voteTarget is None:
+                self.voteTarget = candidateIndex
+
+                self._saveNodeState()
+
+
+
+
+
+                # todo store vote choice on disk
+                willVote = True
+
+
+
+        return RpcReturn(self.currTerm, willVote)
+
+    def call_request_vote(self, otherNode):
+        assert self.isCandidate
+        nodeConn = rpyc.connect(otherNode.host, otherNode.port)
+        requestVoteRetVal = nodeConn.request_vote(self.currTerm, self.identityIndex)
+        return requestVoteRetVal
+
+
+
+    def check_for_election_timeout(self):
+        self.nodeLogger.debug("checking whether election should be started")
+        #todo check if already leader (edge case)
+        with self._lastContactTimestampLock:
+            if (time.time() - self.lastContactTimestamp) > self.electionTimeout:
+                self.isCandidate = True
+                self.currTerm += 1
+                self.lastContactTimestamp = time.time()
+
+        if self.isCandidate:
+            self.nodeLogger.debug("starting election!")
+            # todo start election
+            numVotes = 1
+            numNodes = 1 + len(self.otherNodes)
+
+            for otherNode in self.otherNodes:
+
+
+                if not self.isCandidate:
+                    break
+
+                #possible race condition with _leaderStatus?
+                if numVotes > numNodes/2:
+                    self._leaderStatus = True
+                    self.isCandidate = False
+
+                    heartbeatTimer = threading.Timer(RaftNode.HEARTBEAT_INTERVAL, self.send_heartbeats)
+                    heartbeatTimer.start()
+
+                    break
+
+
+
+    def send_heartbeats(self):
+        assert self.exposed_is_leader()
+
+
+        pass
+        #todo also reset leader's election timer?
 
 
 if __name__ == '__main__':
